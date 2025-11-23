e33283edb4a546511cfab55f09604c35081f059e
Edward Howell <edwardforschool@gmail.com>
2025-11-23 11:33:59 -0500

    Added query0, query 4 now gets more data about the company

---
+++ project_queries/query0.sql
@@ -0,0 +1,27 @@
+
+-- Date Posted
+SELECT
+    EXTRACT(MONTH FROM job_posted_date) AS month_posted,
+    COUNT(job_posted_date)
+FROM job_postings_fact
+GROUP BY month_posted
+ORDER BY month_posted
+
+SELECT
+    EXTRACT(YEAR FROM job_posted_date) AS year_posted,
+    COUNT(job_posted_date)
+FROM job_postings_fact
+GROUP BY year_posted
+ORDER BY year_posted
+
+
+
+-- Work From Home
+SELECT
+    CASE
+        WHEN job_work_from_home THEN 'Work From Home'
+        ELSE 'In Person'
+    END,
+    COUNT(*)
+FROM job_postings_fact
+GROUP BY job_work_from_home
--- project_queries/query4.sql
+++ project_queries/query4.sql
@@ -1,25 +1,30 @@
-WITH jobs_with_skill AS(
+WITH company_jobs AS (
     SELECT
-        DISTINCT job_id
-    FROM skills_job_dim AS skills_job
-    INNER JOIN skills_dim
-        ON skills_job.skill_id = skills_dim.skill_id
-    WHERE
-        skills IN ('python', 'r', 'sql', 'sas', 'excel', 'power bi', 'matlab')
+        company_id,
+        ROUND(AVG(salary_year_avg)) AS average_salary,
+        COUNT(job_postings.company_id) AS job_count
+    FROM job_postings_fact AS job_postings
+    WHERE 
+        job_location LIKE '%MD' AND
+        job_title_short = 'Data Scientist' AND
+        job_id IN (
+            SELECT
+                DISTINCT job_id
+            FROM skills_job_dim AS skills_job
+            INNER JOIN skills_dim
+                ON skills_job.skill_id = skills_dim.skill_id
+            WHERE
+                skills IN ('python', 'r', 'sql', 'sas', 'excel', 'power bi', 'matlab')
+        )
+    GROUP BY company_id
+    ORDER BY job_count DESC
 )
 
-
-SELECT
+SELECT 
     company_dim.name AS company_name,
-    ROUND(AVG(salary_year_avg)) AS average_salary,
-    COUNT(job_postings.company_id) AS job_count
-FROM jobs_with_skill
-INNER JOIN job_postings_fact AS job_postings
-    ON jobs_with_skill.job_id = job_postings.job_id
-INNER JOIN company_dim ON
-    job_postings.company_id = company_dim.company_id
-WHERE 
-    job_location LIKE '%MD' AND
-    job_title_short = 'Data Scientist'
-GROUP BY company_dim.name
-ORDER BY job_count DESC
+    average_salary,
+    job_count,
+    link_google
+FROM company_jobs
+INNER JOIN company_dim
+    ON company_jobs.company_id = company_dim.company_id
